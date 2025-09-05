8a33ec5a3a24da40aa56660d1cc0d5e9fde01c80
arti <amrti79714@gmail.com>
2025-09-05 18:37:35 +0530

    cent ,custom error

--- try_catch.js
+++ try_catch.js
@@ -9,10 +9,7 @@ const db = mysql.createPool({
   password: "",      
   database: "school_db" 
 });
-app.use((err,req,res,next)=>{
-        console.error(err.stack);
-        res.status(500).json({error:"something went wrong"});
-        });
+
 app.get("/try",async(req,res)=>{
     //console.log(req);
     try{
@@ -20,8 +17,43 @@ app.get("/try",async(req,res)=>{
         res.json(result);
     }
     catch(error){
-      res.status(500).json({error:"database error",details:"error.message"});
+      res.status(500).json({error:"database error",details:error.message});
     }  
     })
+
+
+//centrelized error 
+const asynchandler=(fn)=>(req,res,next)=>{
+  Promise.resolve(fn(req,res,next))
+  .catch(next);
+};
+app.get("/central",asynchandler(async(req,res)=>{
+  console.log("in ghcgfc")
+throw new Error ("manual error test");
+  const rows=await db.query("select * from students");
+
+  res.json(rows);
+})
+);
+
+
+///custom error 
+class AppError extends Error{
+  constructor (message,statusCode){
+    super(message);
+    this.statusCode=statusCode;
+  }
+}
+app.get("/custom",(req,res,next)=>{
+//  console.log(req);
+  return next(new AppError("resource not found",404));
+})
+
+app.use((err,req,res,next)=>{
+        console.error("hgcfchcfhfc",err.message);
+        res.status(500).json({error:"something went wrong"});
+        });
+
+        
 app.listen(3000, () =>{
      console.log("Server started on port 3000")});
